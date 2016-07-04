a47d0ce2ca200ee55dc3a18ccc10afa54ffe913e
Ruijie Zhang <jerrydream123@gmail.com>
2016-07-03 21:32:36 -0700

    finish building apis

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var app = express();
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 var port = process.env.PORT || 3000;
 
 app.use('/asset', express.static(__dirname + '/public'));
@@ -11,7 +12,7 @@ app.set('view engine', 'ejs');
 mongoose.connect(config.getConnectionString());
 
 setupController(app);
-
+apiController(app);
 
 
 app.listen(port);
